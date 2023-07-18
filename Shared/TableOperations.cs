bc0edaf2ec7e794e3e94eb6444bdedf4cc7a07df
Drac Zhang <yuzhang6@microsoft.com>
2023-07-18 14:15:06 +0800

    minor changes

--- Shared/TableOperations.cs
+++ Shared/TableOperations.cs
@@ -45,5 +45,24 @@ namespace LogicAppAdvancedTool
 
             return tableEntities;
         }
+
+        public static List<TableEntity> QueryTable(string tableName, string query, string[] select = null)
+        {
+            List<TableEntity> tableEntities = new List<TableEntity>();
+
+            TableServiceClient serviceClient = new TableServiceClient(AppSettings.ConnectionString);
+            Pageable<TableItem> results = serviceClient.Query(filter: $"TableName eq '{tableName}'");
+
+            if (results.Count() == 0)
+            {
+                throw new UserInputException($"Table - {tableName} not exist, please check whether parameters are correct or not.");
+            }
+
+            TableClient tableClient = new TableClient(AppSettings.ConnectionString, tableName);
+
+            tableEntities = tableClient.Query<TableEntity>(filter: query, select: select).ToList();
+
+            return tableEntities;
+        }
     }
 }
